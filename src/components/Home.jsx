393af64a09c486f1545754d23daaa9dc6948df83
unknown <shiveshtiwari0@gmail.com>
2024-02-26 13:57:53 +0530

    todo update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,8 @@ import { PiNewspaperClipping, PiNewspaperLight } from "react-icons/pi";
 import CalendarComponent from "./common/Calendar";
 import TodoCheckbox from "./common/TodoCheckbox";
 import { CheckIcon, WarningTwoIcon } from "@chakra-ui/icons";
+import { GoVerified } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 const theme = extendTheme({
   styles: {
@@ -258,18 +260,23 @@ function Home() {
           </Card>
           <Card width={"35%"}>
             <CardBody>
+              <div className="flex justify-between">
+                <h1 className="text-lg flex gap-2 items-center"><GoVerified size={24} color="#ccc" /> My To Do Items</h1>
+                <Link to="/todo" className="text-blue-400 text-sm">View all</Link>
+              </div>
+              <Divider my={4} />
               <div className="w-full flex flex-col">
                 <h2 className="text-lg mb-2 text-yellow-600 flex items-center gap-2"><WarningTwoIcon /> Latest to do&apos;s</h2>
-              {todoNotDone.map((item, index) => (
-                <TodoCheckbox key={`done-${index}`} done={false} title={item.title} />
-              ))}
+                {todoNotDone.map((item, index) => (
+                  <TodoCheckbox key={`done-${index}`} done={false} title={item.title} />
+                ))}
               </div>
               <Divider my={6} />
               <div className="w-full flex flex-col">
                 <h2 className="text-lg mb-2 text-green-500 flex items-center gap-2"><CheckIcon /> Latest finished to do&apos;s</h2>
-              {todoDone.map((item, index) => (
-                <TodoCheckbox key={`done-${index}`} done={true} title={item.title} />
-              ))}
+                {todoDone.map((item, index) => (
+                  <TodoCheckbox key={`done-${index}`} done={true} title={item.title} />
+                ))}
               </div>
             </CardBody>
           </Card>
