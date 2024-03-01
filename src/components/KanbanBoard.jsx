8045e61b8dae775218216b343d5542c9214adcee
unknown <shiveshtiwari0@gmail.com>
2024-03-02 02:57:42 +0530

    dragging kanban init

---
+++ src/components/KanbanBoard.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CustomKanban } from "./kanban/Board";
+
+const KanbanBoard = () => {
+    const [leads, setLeads] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchLeads = async () => {
+        try {
+            const response = await axios.get(
+                `${import.meta.env.VITE_API_BASE}/api/admin/getAllLeads`
+            );
+            setLeads(response.data);
+            setLoading(false); // Set loading to false once data is fetched
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setLoading(false); // Set loading to false in case of error too
+        }
+    }
+
+    useEffect(() => {
+        fetchLeads();
+    }, [])
+
+    console.log(leads)
+
+    if (loading) return <>Loading...</>
+
+    return (
+        <div>
+            {/* {leads.map((lead, index) => (
+                <p key={`lead-${index}`}>{lead.clientName}</p>
+            ))} */}
+            <CustomKanban data={leads} />
+        </div>
+    )
+}
+
+export default KanbanBoard
