37a42c20d7167f4960bf73fbf951907ff4ff57eb
DAVIN HENRY MONTEIRO <davinmonteiro@gmail.com>
2024-03-07 00:21:34 +0530

    fixed invoice

--- src/components/forms/Invoice.jsx
+++ src/components/forms/Invoice.jsx
@@ -1,205 +1,261 @@
-import { Button, Flex, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react'
-import MyDatePicker from '../common/MyDatePicker'
-import { useEffect, useState } from 'react';
-import moment from 'moment';
-import { TimePicker } from 'antd';
-import SelectProduct from '../common/SelectProduct';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import SelectClient from '../common/SelectClient';
+import React, { useState, useEffect } from "react";
+import {
+  Select,
+  Stack,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const Invoice = () => {
-    const [selectProductValue, setSelectProductValue] = useState({});
-    const [selectedSource, setSelectedSource] = useState({});
-    const [serviceDescription, setServiceDescription] = useState("");
-    const [unitPrice, setUnitPrice] = useState(0);
-    const [quantity, setQuantity] = useState(0);
-    const [clients, setClients] = useState({});
-    const [invoiceData, setInvoiceData] = useState({
-        services: {
-            product: "",
-            serviceDescription: "",
-            duration: "",
-            quantity: 0,
-            unitPrice: unitPrice,
-            startDate: "",
-            endDate: ""
-        },
-        client_id: "",
-        gst: 0,
-        date1: moment().format("DD-MM-YYYY"),
-        time1: moment().format("hh:mm:ss"),
-    });
+  const [clients, setClients] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectedClient, setSelectedClient] = useState("");
+  const [selectedGst, setSelectedGst] = useState(0);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [services, setServices] = useState([]);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const res = await axios.get(`${import.meta.env.VITE_API_BASE}/api/admin/getProductDetails/${selectProductValue}`);
-            setSelectedSource(res.data);
-            setUnitPrice(selectedSource.unitPrice)
-        }
-        fetchProduct();
-        console.log(selectedSource)
-    }, [])
+  useEffect(() => {
+    fetchClients();
+    fetchProducts();
+  }, []);
 
+  const fetchClients = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE}/api/admin/getAllClients`
+      );
+      const data = await response.json();
+      setClients(data);
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const productData = await axios.get(`${import.meta.env.VITE_API_BASE}/api/admin/getProductDetails/${selectProductValue}`);
-        setInvoiceData({
-            ...invoiceData,
-            client_id: `${clients}`,
-            services: {
-                product: productData.data.product,
-                serviceDescription: serviceDescription,
-                duration: "",
-                quantity: quantity,
-                unitPrice: productData.data.unitPrice,
-                startDate: "",
-                endDate: ""
-            }
-        })
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE}/api/admin/getAllProducts`
+      );
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
 
-        const formData = new FormData();
-        console.log(invoiceData)
-        console.log(formData)
+  const handleClientChange = (event) => {
+    const _id = event.target.value;
+    const selectedClient = clients.find((client) => client._id === _id);
+    setSelectedClient(selectedClient);
+  };
 
-        Object.entries(invoiceData).forEach(([key, value]) => {
-            if (value !== "") {
-                formData.append(key, value);
-            }
-        });
-
-
-        axios
-            .post(`${import.meta.env.VITE_API_BASE}/api/admin/createInvoice`, formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            })
-            .then((response) => {
-                if (response.status === 200) {
-                    toast.success(response.data.message);
-                } else {
-                    console.error("Failed to create project");
-                    toast.success(response.data.message);
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-                toast.error(error.response.data.message);
-            });
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setInvoiceData({ ...invoiceData, [name]: value });
-    };
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="flex gap-3">
-          <FormControl id="date1" isRequired>
-            <FormLabel>Invoice Date</FormLabel>
-            <MyDatePicker
-              selected={invoiceData.date1}
-              onChange={(date) =>
-                setInvoiceData({ ...invoiceData, date1: `${date.toDate()}` })
-              }
-              defaultValue={moment()}
-              format={"DD/MM/YYYY"}
-            />
-          </FormControl>
-          <FormControl id="time1" isRequired>
-            <FormLabel>Enquiry Date</FormLabel>
-            <TimePicker
-              selected={invoiceData.time1}
-              onChange={(time) =>
-                setInvoiceData({ ...invoiceData, time1: `${time}` })
-              }
-              defaultValue={moment()}
-              format={"hh:mm:ss"}
-              disabled
-            />
-          </FormControl>
-          <FormControl id="client_id" isRequired maxWidth={300}>
-            <FormLabel>Client Name</FormLabel>
-            <SelectClient
-              selectSourceValue={clients}
-              setSelectSourceValue={setClients}
-            />
-          </FormControl>
-        </div>
-        <div className="flex gap-3 mt-3">
-          <FormControl id="gst" isRequired>
-            <FormLabel>Gst</FormLabel>
-            <Input name="gst" onChange={handleChange} isRequired />
-          </FormControl>
-          <FormControl id="unitPrice" isRequired>
-            <FormLabel>Unit Price</FormLabel>
-            <Input
-              value={unitPrice}
-              onChange={(e) => setUnitPrice(e.target.value)}
-              isRequired
-            />
-          </FormControl>
-          <FormControl id="quantity" isRequired>
-            <FormLabel>Quantity</FormLabel>
-            <Input
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-              isRequired
-            />
-          </FormControl>
-        </div>
-        <div className="mt-3">
-          <FormLabel>Service Description</FormLabel>
-          <Input
-            className="h-16"
-            value={serviceDescription}
-            onChange={(e) => setServiceDescription(e.target.value)}
-          />
-        </div>
-        <div className="flex gap-3 mt-3">
-          <FormControl id="tags"  isRequired>
-            <FormLabel>Product</FormLabel>
-            <Flex>
-              <SelectProduct
-                selectSourceValue={selectProductValue}
-                setSelectSourceValue={setSelectProductValue}
-              />
-            </Flex>
-          </FormControl>
-          <FormControl id="startDate">
-            <FormLabel>Start Date</FormLabel>
-            <MyDatePicker
-              format={"DD/MM/YYYY"}
-              onChange={(date) => {
-                setInvoiceData({
-                  ...invoiceData,
-                  services: { ...invoiceData.services, startDate: date },
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl id="endDate">
-            <FormLabel>End Date</FormLabel>
-            <MyDatePicker
-              format={"DD/MM/YYYY"}
-              onChange={(date) => {
-                setInvoiceData({
-                  ...invoiceData,
-                  services: { ...invoiceData.services, endDate: date },
-                });
-              }}
-            />
-          </FormControl>
-        </div>
-
-        <Button type="submit" colorScheme="purple" className="mt-5">
-          Create Invoice
-        </Button>
-      </form>
+  const handleProductSelect = (event) => {
+    const productId = event.target.value;
+    const selectedProduct = products.find(
+      (product) => product._id === productId
     );
-}
+    setSelectedProducts([...selectedProducts, selectedProduct]);
+  };
 
-export default Invoice
+  const handleServiceChange = (index, field, value) => {
+    const updatedServices = [...services];
+    if (field === "productId") {
+      const selectedProduct = products.find((product) => product._id === value);
+
+      updatedServices[index]["product"] = selectedProduct;
+    } else {
+      updatedServices[index][field] = value;
+    }
+    setServices(updatedServices);
+  };
+
+  const handleAddService = () => {
+    setServices([
+      ...services,
+      {
+        product: "",
+        serviceDescription: "",
+        duration: "",
+        quantity: 1,
+        unitPrice: 0,
+        startDate: "",
+        endDate: "",
+      },
+    ]);
+  };
+
+  const handleRemoveService = (index) => {
+    const updatedServices = [...services];
+    updatedServices.splice(index, 1);
+    setServices(updatedServices);
+  };
+
+  const handleSubmit = async () => {
+    const requestData = {
+      client_id: selectedClient.client_id,
+      gst: parseInt(selectedGst),
+      services: services.map((service) => ({
+        product: service.product.product,
+        serviceDescription: service.serviceDescription,
+        duration: service.duration,
+        quantity: service.quantity,
+        unitPrice: service.product.unitPrice,
+        startDate: service.startDate.toISOString(),
+        endDate: service.endDate.toISOString(),
+      })),
+    };
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE}/api/admin/createInvoice`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+        const file = new Blob([response.data], { type: 'application/pdf' });
+        const fileURL = URL.createObjectURL(file);
+        const link = document.createElement('a');
+        link.href = fileURL;
+        link.setAttribute('download', 'invoice_slip.pdf'); 
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+
+        if (response.status === 200) {
+          toast.success('Invoice Slip is downloaded successfully.');
+        } else {
+          console.error("Failed to download Invoice slip");
+          toast.error('Failed to download Invoice slip.');
+        }
+    } catch (error) {
+      console.error("Error creating invoice:", error);
+    }
+  };
+
+  return (
+    <Stack spacing={4}>
+      <FormControl>
+        <FormLabel>Select Client</FormLabel>
+        <Select placeholder="Select client" onChange={handleClientChange}>
+          {clients.map((client) => (
+            <option key={client._id} value={client._id}>
+              {client.brandName}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
+      {selectedClient && (
+        <>
+          <FormLabel>Client Name:-{selectedClient.clientName}</FormLabel>
+          <FormLabel>Client Company:-{selectedClient.companyName}</FormLabel>
+        </>
+      )}
+      <FormControl>
+        <FormLabel>GST</FormLabel>
+        <Input
+          type="number"
+          placeholder="Enter GST"
+          value={selectedGst}
+          onChange={(e) => setSelectedGst(e.target.value)}
+        />
+      </FormControl>
+
+      <Stack spacing={4}>
+        {services.map((service, index) => (
+          <Stack key={index} spacing={4}>
+            <FormControl>
+              <FormLabel>Select Product</FormLabel>
+              <Select
+                placeholder="Select product"
+                onChange={(e) =>
+                  handleServiceChange(index, "productId", e.target.value)
+                }
+                value={service.productId}
+              >
+                {products.map((product) => (
+                  <option key={product._id} value={product._id}>
+                    {product.product} - Unit Price - {product.unitPrice}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Service Description</FormLabel>
+              <Input
+                value={service.serviceDescription}
+                onChange={(e) =>
+                  handleServiceChange(
+                    index,
+                    "serviceDescription",
+                    e.target.value
+                  )
+                }
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Start Date</FormLabel>
+              <DatePicker
+                selected={service.startDate}
+                onChange={(date) =>
+                  handleServiceChange(index, "startDate", date)
+                } // Corrected to use 'date' instead of 'startDate'
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>End Date</FormLabel>
+              <DatePicker
+                selected={service.endDate}
+                onChange={(date) => handleServiceChange(index, "endDate", date)} // Corrected to use 'date' instead of 'endDate'
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Quantity</FormLabel>
+              <Input
+                type="number"
+                value={service.quantity}
+                onChange={(e) =>
+                  handleServiceChange(index, "quantity", e.target.value)
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Duration</FormLabel>
+              <Input
+                type="text"
+                value={service.duration}
+                onChange={(e) =>
+                  handleServiceChange(index, "duration", e.target.value)
+                }
+              />
+            </FormControl>
+
+            <Button
+              colorScheme="red"
+              onClick={() => handleRemoveService(index)}
+            >
+              Remove Service
+            </Button>
+          </Stack>
+        ))}
+
+        <Button onClick={handleAddService}>Add Service</Button>
+      </Stack>
+
+      <Button onClick={handleSubmit}>Create Invoice</Button>
+    </Stack>
+  );
+};
+
+export default Invoice;
