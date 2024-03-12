888bb3c4fcea017c4755a84a2a4077aa30df5711
DAVIN HENRY MONTEIRO <davinmonteiro@gmail.com>
2024-03-13 00:11:43 +0530

    added more dates

--- src/components/GetAllInvoices.jsx
+++ src/components/GetAllInvoices.jsx
@@ -96,6 +96,16 @@ const GetAllInvoices = () => {
             <option value="" disabled>
               Select Financial Year
             </option>
+            <option value="2034">2034-2035</option>
+            <option value="2033">2033-2034</option>
+            <option value="2032">2032-2033</option>
+            <option value="2031">2031-2032</option>
+            <option value="2030">2030-2031</option>
+            <option value="2029">2029-2030</option>
+            <option value="2028">2028-2029</option>
+            <option value="2027">2027-2028</option>
+            <option value="2026">2026-2027</option>
+            <option value="2025">2025-2026</option>
             <option value="2024">2024-2025</option>
             <option value="2023">2023-2024</option>
             <option value="2022">2022-2023</option>
