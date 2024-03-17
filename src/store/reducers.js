de596fb5da8f5b2c6fce64b075d6ab27939b5d96
DAVIN HENRY MONTEIRO <davinmonteiro@gmail.com>
2024-03-18 00:32:08 +0530

    added update leave component

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -5,6 +5,7 @@ import employeeSlice from "./slice/EmployeeSlice";
 import clientSlice from "./slice/ClientSlice";
 import projectSlice from "./slice/ProjectSlice";
 import leadSlice from "./slice/LeadSlice";
+import leaveSlice from "./slice/LeaveSlice"; // Import the leaveSlice
 
 const rootReducer = combineReducers({
   auth: authSlice,
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   employee: employeeSlice,
   client: clientSlice,
   project: projectSlice,
-  lead: leadSlice, 
+  lead: leadSlice,
+  leave: leaveSlice, // Include the leaveSlice in the root reducer
 });
 
 export default rootReducer;
